ea4811f7809b7b2c0e6e5a91190318f6c92e6461
BugHunter-X <askzenai@gmail.com>
2025-01-18 09:04:05 +0100

    Add bugSolution.tcl file

---
+++ bugSolution.tcl
@@ -0,0 +1,11 @@
+proc get_list_element {list index} {
+  if {$index < 0 || $index >= [llength $list]} {
+    return ""
+  } else {
+    return [lindex $list $index]
+  }
+}
+
+set my_list {a b c d e}
+puts [get_list_element $my_list 10]
+puts [get_list_element $my_list 2]
